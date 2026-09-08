198c7a1f6b5aab26e1c87308840986f737ca08cd
Makrand <makrand.doiphode@gmail.com>
2026-09-08 21:27:12 +0530

    chore: shared eslint/prettier/tsconfig

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+// Root ESLint config shared by all workspaces
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2022: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', 'build', 'node_modules', '.next', 'out', 'coverage'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
+  },
+};
